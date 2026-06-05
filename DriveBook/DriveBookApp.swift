840423373bee0c9bddb70a4126a4abced302fd5e
Thiago Pires <thiagopac@gmail.com>
2026-06-05 13:37:19 -0300

    Initial Commit

---
+++ DriveBook/DriveBookApp.swift
@@ -0,0 +1,18 @@
+//
+//  DriveBookApp.swift
+//  DriveBook
+//
+//  Created by Thiago Castro on 05/06/26.
+//
+
+
+import SwiftUI
+
+@main
+struct DriveBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
